6ea93e5679920c1dd76911267d3fb0442d2dddae
Bartol Bilankov <bartol.bilankov@gmail.com>
2019-07-30 18:45:12 +0100

    working on mobile devices

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Hero.css";
 import TextLoop from "react-text-loop";
+import Media from 'react-media'
 
 const Hero = props => {
   return (
@@ -8,6 +9,8 @@ const Hero = props => {
       <div className={classes.Dark}>
           <div className={classes.Heading}>
               <h2>I Love To Design</h2>
+          <Media query={{minWidth: 768}}>
+            {matches => matches ? (
         <h1>
         I Am
         {" "}<TextLoop springConfig={{ stiffness: 120, damping: 10 }} interval={2500} adjustingSpeed={200} >
@@ -17,6 +20,16 @@ const Hero = props => {
               <span>a Designer</span>
           </TextLoop>{" "}
         </h1>
+            ):(
+              <h1>
+        {" "}<TextLoop springConfig={{ stiffness: 120, damping: 10 }} interval={2500} adjustingSpeed={200} >  
+              <span>Bartol Bilankov</span>
+              <span>I am a Developer</span>
+              <span>I am a Designer</span>
+          </TextLoop>{" "}
+        </h1>
+            )}
+        </Media>
         </div>
         <div className={classes.Icons}>
             <span><a target="_blank" href="https://github.com/MsMatki"><i className="fab fa-github"></i></a></span>
